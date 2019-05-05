08ab5557523902abfee893024095725aa24bcc7c
tkthuc <tkthuc@gmail.com>
2019-05-05 00:16:47 -0400

    Missing package.json

--- frontend/ui/index.tsx
+++ frontend/ui/index.tsx
@@ -6,7 +6,6 @@ import * as ReactDOM from 'react-dom';
 import * as ES6Promise from "es6-promise";
 
 import {BrowserRouter, HashRouter} from "react-router-dom";
-import 'font-awesome/css/font-awesome.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './containers/App';
 
