24c795f247ef6d7a932c1a997b9e30ba42351401
tkthuc <tkthuc@gmail.com>
2019-05-19 23:38:55 -0400

    Remove unused import

--- product-service/src/main/java/com/bookstore/productsevice/controllers/BookController.java
+++ product-service/src/main/java/com/bookstore/productsevice/controllers/BookController.java
@@ -6,8 +6,6 @@ import com.bookstore.productsevice.repository.BookRepository;
 import com.bookstore.productsevice.storage.StorageService;
 import com.bookstore.productsevice.validators.BookValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
