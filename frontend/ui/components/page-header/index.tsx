73293afbdfcdeba4cfa9eb02dbd538cef90dbdce
tkthuc <tkthuc@gmail.com>
2019-05-19 23:31:35 -0400

    Add styles for main page

--- frontend/ui/components/page-header/index.tsx
+++ frontend/ui/components/page-header/index.tsx
@@ -4,6 +4,8 @@ import * as React  from 'react';
 
 import './page-header.css';
 import './search-box.css';
+import './cart-bar.css';
+
 import {useState} from "react";
 
 
@@ -11,7 +13,12 @@ export default function(props) {
 
         let tabs = ["Book for you", "Bestsellers", "New Release"];
 
-        let [activeIndex, setActiveIndex] = useState<number>(0);
+        let topCategories = ["Art & Photography","Children Books","Craft and Hobbies" ]
+
+        let [arrowDirection,setArrowDirection] = useState<string>("down");
+
+
+
         return (
             <header className="site-header">
                 <nav className="navbar">
@@ -51,7 +58,7 @@ export default function(props) {
                 </nav>
                 <div className="search-area">
                     <div className="search-form">
-                         <span className="search-dropdown">
+                        <span className="search-dropdown">
                         <select>
                             <option>All</option>
                             <option>Fiction</option>
@@ -63,6 +70,31 @@ export default function(props) {
                         <input className="search-input" type="text" name="search_input"/>
                         <button>Search</button>
                     </div>
+                </div>
+                <div className="cart-bar">
+                    <ul>
+                        <li className="categories-dropdown"  onMouseLeave={() => setArrowDirection("down")}>
+                            <div onMouseEnter={() => setArrowDirection("up")}>Shop by category <i className={`angle ${arrowDirection} icon`}/></div>
+
+                                    <ul>
+                                        <li><a>Top Categories</a></li>
+                                        {
+                                            topCategories.map((item,index) => {
+                                               return <li key={index}><a> {item}</a></li>
+                                            })
+                                        }
+                                    </ul>
+                        </li>
+                        <li> Bestsellers </li>
+                        <li> Coming soon </li>
+                        <li> New Releases </li>
+                        <li> Bargain shop </li>
+                    </ul>
+                    <ul>
+                        <li> $CAD </li>
+                        <li> C$0.00 </li>
+                        <li> 0 </li>
+                    </ul>
                 </div>
             </header>
         );
